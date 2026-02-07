a0876e7309afe52ff9c4d04331101c7697d056fe
Ricardo Fernández Chamorro <databaandsc@gmail.com>
2026-02-07 18:43:39 +0100

    Initial setup: Spring Boot dependencies and project structure

---
+++ backend/src/main/java/com/barbapp/api/BarbappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.barbapp.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbappBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/barbapp/api/BarbappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbapp.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
